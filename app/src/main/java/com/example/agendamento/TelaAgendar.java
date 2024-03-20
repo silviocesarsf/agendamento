89943bb2248ad8e2a28a6b858d2863ebed27b355
Silvio <silvio14dmc@gmail.com>
2024-03-19 21:34:49 -0300

    feat: nova tela

--- app/src/main/java/com/example/agendamento/TelaAgendar.java
+++ app/src/main/java/com/example/agendamento/TelaAgendar.java
@@ -1,44 +1,38 @@
 package com.example.agendamento;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatSpinner;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
-    private FrameLayout btnAgendar;
+public class TelaAgendar extends AppCompatActivity {
+    private AppCompatSpinner spinnerBarbeiros;
+    private ArrayList BarbeirosDisponiveis = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tela_agendar);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        btnAgendar = findViewById(R.id.btnAgendar);
+        spinnerBarbeiros = findViewById(R.id.spinnerBarbeiros);
+        BarbeirosDisponiveis.add("Marcos");
+        BarbeirosDisponiveis.add("Bruno");
+        BarbeirosDisponiveis.add("Willian");
 
-        btnAgendar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AbrirTela();
-            }
-        });
+        ArrayAdapter<String> adapterBarbeiros = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.select_dialog_item_material, BarbeirosDisponiveis);
+        spinnerBarbeiros.setAdapter(adapterBarbeiros);
     }
-
-    private void AbrirTela() {
-        Intent intent = new Intent(MainActivity.this, TelaAgendar.class);
-        startActivity(intent);
-    };
 }
